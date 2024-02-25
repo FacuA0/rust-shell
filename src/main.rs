1f921d3dec54503a68fd69e6c48861ae58ceea0f
FacuA0 <facundoamarilla28@gmail.com>
2024-02-25 08:41:09 -0300

    Current path shown and split input in args

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
 use std::io::{self, Write};
-use std::time::Duration;
-use std::thread;
+use std::env;
 
 fn main() {
     println!("Rust Terminal");
-    println!("@FacuA0\n");
+    println!("Author: @FacuA0\n");
 
     let mut stdout = io::stdout();
 
     loop {
-        print!("> ");
+        let path = env::current_dir();
+        let mut prompt = String::from("> ");
+        if path.is_ok() {
+            let path = String::from(path.unwrap().to_str().unwrap());
+            prompt = path + &prompt;
+        }
+        
+        print!("{prompt}");
         stdout.flush().unwrap();
     
         let mut value = String::new();
         io::stdin().read_line(&mut value).unwrap();
     
-        let value = value.trim();
-        match value {
+        let parts: Vec<&str> = value.trim().split(" ").collect();
+        let command = parts[0];
+        match command {
             "exit" => break,
             _ => ()
         }
